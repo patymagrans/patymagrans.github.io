bee8680cb24d3b0df6bf092cda02bdda442f915f
patymagrans <157524367+patymagrans@users.noreply.github.com>
2024-06-18 17:00:42 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,8 @@ const config = {
     aboutMeTitle: "About me",
 
     biography: "Hello, I'm Agustín Patiño Magrans a. k. a. Paty Magrans. I am an Argentinian sound technician and music producer graduated from ORT Argentina." 
+
+        
         "Here you can find various works I have done in the field of audio. You can find several mixes and masters I have done, ordered from oldest (top) to newest (bottom).",
     
     footer: "Audio Engineering"
