75270bdb6bd90da9a5923fd7e24338b7f149e46e
Lisandro <lisandropat@gmail.com>
2024-07-06 23:54:44 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -72,6 +72,7 @@ const BlogIndex = ({ data, location }) => {
           <h4>{config.moreWorkTitle}</h4>
           <iframe className="soundcloudPlaylist" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1814131524%3Fsecret_token%3Ds-9nbCpjwglDi&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>
           <div className="soundcloudPlaylist-prettier" />
+          <Link to={ "mailto:" + config.authorMail + "?subject=Ask%20for%20WAV:" }>{askWavTitle}</Link>
         </div>
       </ol>
       </>
