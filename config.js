9a7b36b5532d813fa2669b5209cc3ff35fcdf189
Lisandro <lisandropat@gmail.com>
2024-07-06 23:52:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,7 @@ const config = {
     authorMail: "patymagrans@gmail.com",
 
     moreWorkTitle: "More songs mixed by me",
+    askWavTitle: "Feel free to ask for .WAV files",
     aboutMeTitle: "About me",
 
     biography: "Hello, I'm Agustín Patiño Magrans a. k. a. Paty Magrans. I am an Argentinian sound technician and music producer graduated from ORT Argentina. Here you can find various works I have done in the field of audio. You can find several mixes and masters I have done, ordered from newest to oldest.",
