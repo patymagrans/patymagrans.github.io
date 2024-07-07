6399aac902b80b8509c5062e338207f8b408315b
patymagrans <157524367+patymagrans@users.noreply.github.com>
2024-07-06 21:04:44 -0300

    Update config.js

--- config.js
+++ config.js
@@ -7,11 +7,11 @@ const config = {
 
     authorMail: "patymagrans@gmail.com",
 
-    moreWorkTitle: "More songs mixed by me",
+    moreWorkTitle: "Mixed & Mastered by me",
     askWavTitle: "These are .MP3 files (lower quality). Feel free to ask for .WAV files",
     aboutMeTitle: "About me",
 
-    biography: "Hello, I'm Agustín Patiño Magrans a. k. a. Paty Magrans. I am an Argentinian sound technician and music producer graduated from ORT Argentina. Here you can find various works I have done in the field of audio. You can find several mixes and masters I have done, ordered from newest to oldest.",
+    biography: "Hello, I'm Agustín Patiño Magrans a. k. a. Paty Magrans. I am an Argentinian sound technician and music producer graduated from ORT Argentina. Here you will find various works I have done in the field of audio. Below, you can see the mixes and masters I have completed, ordered from newest to oldest.",
     
     footer: "Audio Engineering and Sound Design"
 
